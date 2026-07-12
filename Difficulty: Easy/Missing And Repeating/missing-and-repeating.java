897c970b4d864c9fb18b7e2eade85ea88d603edc
Siddhi Dabholkar <86103727+SiddhiDabholkar10@users.noreply.github.com>
2026-07-12 19:07:18 -0400

    Refactor findTwoElement method for clarity

--- Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
+++ Difficulty: Easy/Missing And Repeating/missing-and-repeating.java
@@ -1,30 +1,28 @@
 class Solution {
-    ArrayList<Integer> findTwoElement(int[] arr) {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        // code here
         int n = arr.length;
-
-        long Sn = (long) n * (n + 1) / 2;
-        long S2n = (long) n * (n + 1) * (2L * n + 1) / 6;
-
+        long Sn = (long) n*(n+1)/2;
+        long S2n = (long) n*(n+1)*(2L*n+1)/6;
         long S = 0;
         long S2 = 0;
-
-        for (int a : arr) {
+        for(int a : arr){
             S += a;
-            S2 += (long) a * a;
+            S2 += (long) a*a;
         }
-
-        long val1 = S - Sn;       // repeating - missing
-        long val2 = S2 - S2n;     // repeating² - missing²
-
-        val2 = val2 / val1;       // repeating + missing
-
-        long repeating = (val1 + val2) / 2;
-        long missing = repeating - val1;
-
+        long val1 = S - Sn;  //(x-y)
+        long val2 = S2 - S2n;   //(x2 - y2)
+        val2 = val2/val1; //(x+y)
+        //2x = val1+val2 -> x = (val1+val2)/2
+        long x = (val1 + val2) / 2;
+        long y = x - val1;
+        
         ArrayList<Integer> ans = new ArrayList<>();
-        ans.add((int) repeating);
-        ans.add((int) missing);
-
+        ans.add((int) x);
+        ans.add((int) y);
+        
         return ans;
+        
+        
     }
 }
